3eacfb33dc79d55f3c8ea84d78e6b1ef4e42777d
Johan Andersson <johan.peter.andersson@gmail.com>
2016-02-29 22:35:01 +0100

    Add tests

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
+module Main where
+
 import qualified HappyParser
 import AST
+import Test.HUnit
 
 runEvalWith :: (String -> [Statement]) -> String -> IO ()
 runEvalWith parseExpr input = do
@@ -14,3 +17,32 @@ main = do
   putStrLn input
   putStrLn "\nUsing Happy:"
   runEvalWith HappyParser.parseExpr input
+
+parse = show . HappyParser.parseExpr 
+
+
+
+
+testParse input = show $ HappyParser.parseExpr input
+
+mapTuple f (a1, a2) = (f a1 a2)
+
+inputPars = [("bla", "tmp")]
+
+makeTests inputPairs = mapTuple doTest inputPairs
+
+tests = do
+    test1 <- (doTest "bla" "tmp")
+    return $ TestList [TestLabel "test1" test1]
+
+doTest expectedPath inputPath = do
+    input <- readFile inputPath
+    expected <- readFile expectedPath
+    let a = TestCase (assertEqual inputPath expected $ testParse input)
+    return a
+
+verify :: IO ()
+verify = do 
+    tests1 <- tests
+    _ <- runTestTT $ tests1
+    return ()
--- test/MainTest.hs
+++ test/MainTest.hs
@@ -1,9 +1,12 @@
 module Main where
 
+import AST
 import Test.HUnit
+
 tests = TestList [TestLabel "test1" test1]
 test1 = TestCase (assertEqual "for (foo 3)," (1 + 2) (3))
 
 main :: IO ()
 main = do _ <- runTestTT $ tests
           return ()
+
