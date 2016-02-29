c3075783ae9147be51ac7fc2cdcac11f445125db
Johan Andersson <johan.peter.andersson@gmail.com>
2016-02-29 21:41:46 +0100

    Add hunit skeleton

---
+++ test/MainTest.hs
@@ -0,0 +1,9 @@
+module Main where
+
+import Test.HUnit
+tests = TestList [TestLabel "test1" test1]
+test1 = TestCase (assertEqual "for (foo 3)," (1 + 2) (3))
+
+main :: IO ()
+main = do _ <- runTestTT $ tests
+          return ()
