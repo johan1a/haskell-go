d09c9616a44bb25f73a609fd7ad50647f2ccc87e
Johan Andersson <johan.peter.andersson@gmail.com>
2016-06-18 16:46:04 +0200

    Main reads from cmd arguments

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import qualified HappyParser
+import System.Environment
 import AST
 import Test.HUnit
 import Verify
@@ -11,8 +12,8 @@ runEvalWith parseExpr input = do
   let ast = parseExpr input
   putStrLn $ "AST: " ++ (show ast)
 
-main :: IO ()
-main = do
+badRepl :: IO ()
+badRepl = do
   putStrLn "Enter stuff:"
   input <- getLine  
   putStrLn "Input:"
@@ -20,6 +21,12 @@ main = do
   runEvalWith HappyParser.parseExpr input
   return ()
 
+main :: IO ()
+main = do 
+    args <- getArgs
+    content <- readFile $ args !! 0
+    putStrLn content
+    return ()
 
 parse :: String -> String
 parse = show . HappyParser.parseExpr 
