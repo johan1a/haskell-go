f20f995daf7c828c82572559b1bde5939578c46d
Johan Andersson <johan.peter.andersson@gmail.com>
2016-10-11 20:02:11 +0200

    refactor

--- src/Eval.hs
+++ src/Eval.hs
@@ -24,11 +24,11 @@ eval = evalIn empty
 --state, program state
 
 runProgram :: [Statement] -> IO (State)
-runProgram stmts = return $ execStmts empty stmts
+runProgram stmts = return $ execStmts stmts empty
 
-execStmts :: State -> [Statement] -> State
-execStmts state [] = state
-execStmts state (s:ss) = execStmts (execStmt s state) ss
+execStmts :: [Statement] -> State -> State
+execStmts [] state = state
+execStmts (s:ss) state = execStmts ss (execStmt s state)
 
 execStmt :: Statement -> State -> State
 execStmt (Expr e) =  error "todo"
@@ -39,9 +39,9 @@ execStmt (IfStmt ifStmt) = error "TODO"
 
 --TODO implement types, multiple declarations
 execDecl :: Declaration -> State -> State
-execDecl (ConstDecl idDecls type_ exprs) state = bind state (getName $ idDecls !! 0) (exprs !! 0)
+execDecl (ConstDecl idDecls type_ exprs) state = bind (getName $ idDecls !! 0) (exprs !! 0) state 
 execDecl (TypeDecl name type_) state = error "Types not implemented"
-execDecl (VarDecl idDecls type_ exprs) state = bind state (getName $ idDecls !! 0) (exprs !! 0)
+execDecl (VarDecl idDecls type_ exprs) state = bind (getName $ idDecls !! 0) (exprs !! 0) state 
 
 execSimpleStmt :: SimpleStmt -> State -> State
 execSimpleStmt (Assignment a) = executeAssign a
@@ -50,13 +50,13 @@ execSimpleStmt (Assignment a) = executeAssign a
 -- TODO assigns first in lhs to first in rhs.
 -- does not yet support multiple declarations at once
 executeAssign :: Assignment -> State -> State
-executeAssign (Assign lhs rhs) state = bindAssign state (lhs !! 0) (rhs !! 0)
+executeAssign (Assign lhs rhs) state = bindAssign (lhs !! 0) (rhs !! 0) state 
 executeAssign _ state = error "ey"
 
 
-bindAssign :: State -> Expr -> Expr -> State
-bindAssign state (IdUse name) rhs = bind state name $ eval state rhs
-bindAssign state _ rhs = error "TODO"
+bindAssign :: Expr -> Expr -> State -> State
+bindAssign (IdUse name) rhs state = bind name (eval rhs state) state
+bindAssign _ rhs state = error "TODO"
 
 type State = Map Name Expr
 
@@ -66,8 +66,8 @@ empty = Map.empty
 
 getName (IdDecl name) = name
 
-bind :: State -> Name -> Expr -> State
-bind state name val = Map.insert name val state
+bind :: Name -> Expr -> State -> State
+bind name val state = Map.insert name val state
 
 env state x = fromJust $ Map.lookup x state
 
@@ -78,13 +78,13 @@ lookup1 state (IdUse x) = env state x
 lookup1 _ (Num n) = error "not an idUse"
 
 
-eval :: State -> Expr -> Expr
-eval state (IdUse x) = eval state $ lookup1 state (IdUse x)
-eval state (BinExpr e ) = evalBin state e
-eval _ (Num n) = Num n 
+eval :: Expr -> State -> Expr
+eval (IdUse x) state  = eval (lookup1 state (IdUse x)) state
+eval (BinExpr e ) state = evalBin e state
+eval (Num n) _ = Num n 
 
-evalBin :: State -> BinExpr -> Expr
-evalBin state (AddExpr l r) = add (eval state l) (eval state r) 
+evalBin :: BinExpr -> State -> Expr
+evalBin (AddExpr l r) state = add (eval l state) (eval r state) 
 evalBin _ _ = error "ToDO"
 
 add :: Expr -> Expr -> Expr
